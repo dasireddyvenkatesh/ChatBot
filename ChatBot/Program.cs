7ff13d2944f4a59e0c72bacdbf461bc9c5c641ac
Venkatesh Dasireddy <dasireddyvenkatesh@gmail.com>
2024-06-01 14:30:21 +0530

    Commented Hsts

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -49,7 +49,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    //app.UseHsts();
 }
 
 app.UseHttpsRedirection();
